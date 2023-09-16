f4f92ee6600a625e71f9775634d08dbb8e7d4784
JhonAndersonCuarrtasRestrepo <cuartas7697@gmail.com>
2023-09-16 16:42:30 -0500

    Proyecto integrador: Base de datos

---
+++ app.js
@@ -0,0 +1,20 @@
+const listaDeTareasBd =[
+        {
+    "titulo": "Mercar",
+    "descripcion": "Comprar leche, huevos, harina, aceite y pan en el supermercado",
+    "fecha_de_vencimiento": "2023-09-18",
+    "estado": "Pendiente"
+        },
+        {
+    "titulo": "Reunión de trabajo",
+    "descripcion": "Validar indicadores del equipo para implementar un plan de accion",
+    "fecha_de_vencimiento": "2023-09-16",
+    "estado": "En progreso"
+        },
+        {
+    "titulo": "Hacer ejercicio",
+    "descripcion": "Correr durante 30 minutos, levantar pesas, hacer sentadillas",
+    "fecha_de_vencimiento": "2023-09-19",
+    "estado": "Completada"
+        }
+];
---
+++ documento_de_google.md
@@ -0,0 +1,3 @@
+Enlace del documento de googe:
+
+https://docs.google.com/document/d/1m9SYnaqGNk1A-PF4mjVoBhafCPHZs6iuNElsZcubCjM/edit?usp=sharing
